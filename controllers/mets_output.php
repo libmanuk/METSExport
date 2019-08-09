4c8897be2f4f8f86eed8b64a8c31f851aabc1e96
Eric C. Weig <eweig@email.uky.edu>
2019-08-09 12:48:31 -0400

    Update mets_output.php

--- controllers/mets_output.php
+++ controllers/mets_output.php
@@ -3,47 +3,47 @@
 // handle fields that need to deal with multiple values separated by ';'
 // Dublin Core:Creator, Dublin Core:Date, Dublin Core:Description, Dublin Core:Coverage, Dublin Core:Format, Dublin Core:Language, Dublin Core:Publisher, Dublin Core:Subject
 
-$dccreator = metadata('item', array('Dublin Core', 'Creator'), array('delimiter'=>';'));
+$dccreator = metadata($item, array("Dublin Core", "Creator"));
 $dccreator = htmlspecialchars($dccreator, ENT_QUOTES);
 $rdccreator = str_replace(";","</dc:creator>\n <dc:creator>",$dccreator);
 $rdccreator = str_replace("> ",">",$rdccreator);
 
-$dccontributor = metadata('item', array('Dublin Core', 'Contributor'), array('delimiter'=>';'));
+$dccontributor = metadata($item, array("Dublin Core", "Contributor"));
 $dccontributor = htmlspecialchars($dccontributor, ENT_QUOTES);
 $rdccontributor = str_replace(";","</dc:contributor>\n <dc:contributor>",$dccreator);
 $rdccontributor = str_replace("> ",">",$rdccontributor);
 
-$dcdate = metadata('item', array('Dublin Core', 'Date'), array('delimiter'=>';'));
+$dcdate = metadata($item, array("Dublin Core", "Date"));
 $dcdate = htmlspecialchars($dcdate, ENT_QUOTES);
 $rdcdate = str_replace(";","</dc:date>\n <dc:date>",$dcdate);
 $rdcdate = str_replace("> ",">",$rdcdate);
 
-$dccoverage = metadata('item', array('Dublin Core', 'Coverage'), array('delimiter'=>';'));
+$dccoverage = metadata($item, array("Dublin Core", "Coverage"));
 $dccoverage = htmlspecialchars($dccoverage, ENT_QUOTES);
 $rdccoverage = str_replace(";","</dc:coverage>\n <dc:coverage>",$dccoverage);
 $rdccoverage = str_replace("> ",">",$rdccoverage);
 
-$dcformat = metadata('item', array('Dublin Core', 'Format'), array('delimiter'=>';'));
+$dcformat = metadata($item, array("Dublin Core", "Format"));
 $dcformat = htmlspecialchars($dcformat, ENT_QUOTES);
 $rdcformat = str_replace(";","</dc:format>\n <dc:format>",$dcformat);
 $rdcformat = str_replace("> ",">",$rdcformat);
 
-$dclanguage = metadata('item', array('Dublin Core', 'Language'), array('delimiter'=>';'));
+$dclanguage = metadata($item, array("Dublin Core", "Language"));
 $dclanguage = htmlspecialchars($dclanguage, ENT_QUOTES);
 $rdclanguage = str_replace(";","</dc:language>\n <dc:language>",$dclanguage);
 $rdclanguagen = str_replace("> ",">",$rdclanguage);
 
-$dcpublisher = metadata('item', array('Dublin Core', 'Publisher'), array('delimiter'=>';'));
+$dcpublisher = metadata($item, array("Dublin Core", "Publisher"));
 $dcpublisher = htmlspecialchars($dcpublisher, ENT_QUOTES);
 $rdcpublisher = str_replace(";","</dc:publsher>\n <dc:publisher>",$dcpublisher);
 $rdcpublisher = str_replace("> ",">",$rdcpublisher);
 
-$dcsubject = metadata('item', array('Dublin Core', 'Subject'), array('delimiter'=>';'));
+$dcsubject = metadata($item, array("Dublin Core", "Subject"));
 $dcsubject = htmlspecialchars($dcsubject, ENT_QUOTES);
 $rdcsubject = str_replace(";","</dc:subject>\n <dc:subject>",$dcsubject);
 $rdcsubject = str_replace("> ",">",$rdcsubject);
 
-$dcdescription = metadata('item', array('Dublin Core', 'Description'), array('delimiter'=>';'));
+$dcdescription = metadata($item, array("Dublin Core", "Description"));
 $dcdescription = htmlspecialchars($dcdescription, ENT_QUOTES);
 $rdcdescription = str_replace(";","</dc:description>\n <dc:description>",$dcdescription);
 $rdcdescription = str_replace("> ",">",$rdcdescription);
@@ -72,4 +72,3 @@ $result[$id]['mets'] = $tag00 . $tag01 . $tag02 . $tag03 . $tag04 . $tag05 . $ta
     $result[$id]['mets'] = str_replace(" </dc:","</dc:",$result[$id]['mets']);
 
 ?>
-
